9e33a68ef42b8898a6ce77d97672bc5dfd282000
Phuc Nguyen <xsensevwx@gmail.com>
2021-09-17 17:58:14 +0700

    Initial commit

---
+++ src/AnyConfigServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace SnowSolution\AnyConfig;
+
+use Illuminate\Support\ServiceProvider;
+
+class AnyConfigServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/views', 'snowexpress');
+        if (!$this->app->routesAreCached()) {
+            require __DIR__ . '/routes.php';
+        }
+    }
+
+    public function register()
+    {
+        //
+    }
+}
---
+++ src/ConfigurationController.php
@@ -0,0 +1,10 @@
+<?php
+namespace SnowSolution\AnyConfig;
+use App\Http\Controllers\Controller;
+
+class ConfigurationController extends Controller {
+    public function index()
+    {
+        die('2222');
+    }
+}
