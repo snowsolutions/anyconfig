3fef984ef4fc21118146f9a4ddda2dc43957d543
Phuc Nguyen <xsensevwx@gmail.com>
2021-09-17 18:25:52 +0700

    Update gitignore

--- src/AnyConfigServiceProvider.php
+++ src/AnyConfigServiceProvider.php
@@ -6,15 +6,25 @@ use Illuminate\Support\ServiceProvider;
 
 class AnyConfigServiceProvider extends ServiceProvider
 {
-    public function boot()
-    {
-        $this->loadViewsFrom(__DIR__ . '/views', 'snowexpress');
-        if (!$this->app->routesAreCached()) {
-            require __DIR__ . '/routes.php';
-        }
-    }
+//    public function boot()
+//    {
+//        $this->loadViewsFrom(__DIR__ . '/views', 'snowexpress');
+//        if (!$this->app->routesAreCached()) {
+//            require __DIR__ . '/routes.php';
+//        }
+//    }
+//
+//    public function register()
+//    {
+//        //
+//    }
 
     public function register()
+    {
+        //
+    }
+
+    public function boot()
     {
         //
     }
